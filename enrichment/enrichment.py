0d88dfe41c2f4f58c2fb217b428da3b7f3a610f0
uahmad417 <18u.ahmad@gmail.com>
2024-12-15 15:28:31 +0200

    enriched data is now published back to the broker

--- enrichment/enrichment.py
+++ enrichment/enrichment.py
@@ -1,10 +1,9 @@
+import json
 import logging
-
-import requests
 import os
 
+import requests
 from paho.mqtt import client as mqtt
-import json
 
 
 class Enrichment:
@@ -18,6 +17,7 @@ class Enrichment:
 
     def __init__(self) -> None:
         self.logger: logging.Logger = self.create_logger()
+        self.producer: mqtt.Client = self.create_mqtt_producer()
         self.subscriber: mqtt.Client = None
 
     def start(self):
@@ -27,7 +27,7 @@ class Enrichment:
 
         broker_host = os.getenv("MQTT_HOST")
         broker_port = os.getenv("MQTT_PORT")
-    
+
         self.subscriber = self.create_mqtt_subscriber()
         self.subscriber.connect(broker_host, int(broker_port))
         self.subscriber.loop_forever()
@@ -54,6 +54,26 @@ class Enrichment:
         logger.info("Starting Enrichment service...")
         return logger
 
+    def create_mqtt_producer(self) -> mqtt.Client:
+        """
+        Configure the mqtt producer
+
+        ## Returns
+
+        A `mqtt.Client` object
+        """
+
+        broker_host = os.getenv("MQTT_HOST")
+        broker_port = os.getenv("MQTT_PORT")
+        client = mqtt.Client()
+        client.tls_set("ca.crt")
+        client.tls_insecure_set(True)
+        client.on_connect = self.__producer_on_connect
+        client.on_publish = self.__producer_on_publish
+        client.connect(broker_host, int(broker_port))
+        client.loop_start()
+        return client
+
     def create_mqtt_subscriber(self) -> mqtt.Client:
         """
         Configure the mqtt subscriber
@@ -66,23 +86,42 @@ class Enrichment:
         client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
         client.tls_set("ca.crt")
         client.tls_insecure_set(True)
-        client.on_connect = self.__on_connect
-        client.on_message = self.__on_message
+        client.on_connect = self.__subscriber_on_connect
+        client.on_message = self.__subscrber_on_message
 
         return client
 
-    def __on_connect(self, client, userdata, flags, reason_code, properties):
+    def __producer_on_connect(self, client, userdata, flags, reason_code):
+        """
+        When connected with mqtt broker
+        """
+
+        if reason_code == 0:
+            self.logger.info(f"Connected with mqtt broker {reason_code}")
+        else:
+            self.logger.error(
+                f"Failed to connect with broker with result: {reason_code}")
+
+    def __producer_on_publish(self, client, userdata, mid):
+        """
+        when messge is published to broker
+        """
+
+        self.logger.debug(f"Msg published {mid}")
+
+    def __subscriber_on_connect(self, client, userdata, flags, reason_code, properties):
         """
         When connected with the broker then subscribe to the topic
         """
 
         if reason_code.is_failure:
-            self.logger.error(f"Failed to connect: {reason_code}. loop_forever() will retry connection")
+            self.logger.error(f"Failed to connect: {
+                              reason_code}. loop_forever() will retry connection")
         else:
             self.logger.info(f"Connected with broker")
             self.subscriber.subscribe("cowrie/#")
 
-    def __on_message(self, client, userdata, message):
+    def __subscrber_on_message(self, client, userdata, message):
         """
         When message is recieved, perform enrichemnt
         """
@@ -100,6 +139,14 @@ class Enrichment:
             response = requests.get(
                 url="https://api.abuseipdb.com/api/v2/check",
                 params={"maxAgeInDays": 90, "ipAddress": src_ip},
-                headers={"Key": os.environ.get("ABUSEIPDB_API_KEY"), "accept": "application/json"}
-                )
-            self.logger.info({response.content})
+                headers={"Key": os.environ.get(
+                    "ABUSEIPDB_API_KEY"), "accept": "application/json"}
+            )
+
+            data["data"].update({"abuse_ipdb": response.json().get("data")})
+            self.producer.publish(
+                topic='enriched',
+                payload=json.dumps(data).encode('utf-8'),
+                qos=0,
+                retain=False
+            )
